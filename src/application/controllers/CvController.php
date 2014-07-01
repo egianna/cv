e8f4a0a29bebb589af13047d13178a5011cac503
egianna <eric.gianna@free.fr>
2014-07-01 09:23:03 +0200

    modif cv pdf

---
+++ src/application/controllers/CvController.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Controller par défaut de l'application, correspond à la page d'index du site
+ * 
+ * @category Application
+ * @author Formateur
+ * @version 0.0.1
+ */
+class CvController extends Controller
+{    
+    public function action()
+    {
+
+    }
+}
